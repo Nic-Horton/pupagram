ab981ef183ac8a086b8260bd4b63def7badde776
Nicolas Horton <nic.horton00@gmail.com>
2023-12-09 17:50:14 -0600

    Set up Homepage skeleton for feed/posts"

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,23 @@
+import { Box, Container, Flex } from "@chakra-ui/react"
+import FeedPosts from "../../components/FeedPosts/FeedPosts"
 
 function HomePage() {
   return (
-    <div>HomePage</div>
+    <Container maxW={"container.lg"}>
+      <Flex gap={20}>
+        <Box flex={2} py={10} border={"1px solid blue"}>
+          <FeedPosts />
+        </Box>
+        <Box flex={3} mr={20}
+          display={{base:"none",lg:"block"}}
+          maxW={"300px"} 
+          border={"1px solid red"}
+        >
+          Suggested
+        </Box>
+      </Flex>
+    </Container>
+    
   )
 }
 
