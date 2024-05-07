5e7180d8cc3644312bb770168fcc5657d6f03c2f
Nicolas Horton <nic.horton00@gmail.com>
2024-05-07 18:22:34 -0500

    Set up follow and unfollow fonctionality on profile pages

--- src/components/Profile/ProfileHeader.jsx
+++ src/components/Profile/ProfileHeader.jsx
@@ -2,13 +2,16 @@ import { Avatar, AvatarGroup, Button, Flex, Text, VStack, useDisclosure } from "
 import useUserProfileStore from "../../store/userProfileStore"
 import useAuthStore from "../../store/authStore";
 import EditProfile from "./EditProfile";
+import useFollowUser from "../../hooks/useFollowUser";
 
 function ProfileHeader() {
   const { userProfile } = useUserProfileStore();
   const authUser = useAuthStore(state => state.user);
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const { isUpdating, isFollowing, handleFollowUser } = useFollowUser(userProfile?.uid)
+
   const visitingOwnProfileAndAuth = authUser && authUser.username === userProfile.username;
   const visitingAnotherProfileAndAuth = authUser && authUser.username !== userProfile.username;
-  const { isOpen, onOpen, onClose } = useDisclosure()
 
   return (
     <Flex gap={{base:4,sm:10}} py={10} direction={{base:"column",sm:"row"}}>
@@ -39,8 +42,11 @@ function ProfileHeader() {
 
           {visitingAnotherProfileAndAuth && (
             <Flex gap={4} alignItems={"center"} justifyContent={"center"}>
-              <Button bg={"blue.500"} color={"white"} _hover={{bg:"blue.600"}} size={{base:"xs",md:"sm"}}>
-                Follow
+              <Button bg={"blue.500"} color={"white"} _hover={{bg:"blue.600"}} size={{base:"xs",md:"sm"}}
+                onClick={handleFollowUser}
+                isLoading={isUpdating}
+              >
+                {isFollowing ? "Unfollow" : "Follow"}
               </Button>
             </Flex>
           )}
---
+++ src/hooks/useFollowUser.js
@@ -0,0 +1,83 @@
+import { useEffect, useState } from 'react';
+import useAuthStore from '../store/authStore';
+import useShowToast from './useShowToast';
+import useUserProfileStore from '../store/userProfileStore';
+import { arrayRemove, arrayUnion, doc, updateDoc } from 'firebase/firestore';
+import { firestore } from '../firebase/firebase';
+
+const useFollowUser = (userId) => {
+	const [isUpdating, setIsUpdating] = useState(false);
+	const [isFollowing, setIsFollowing] = useState(false);
+	const { user, setUser } = useAuthStore();
+	const { userProfile, setUserProfile } = useUserProfileStore();
+	const showToast = useShowToast();
+
+	const handleFollowUser = async () => {
+		setIsUpdating(true);
+		try {
+			const currentUserRef = doc(firestore, 'users', user.uid);
+			const userToFollowOrUnfollowRef = doc(firestore, 'users', userId);
+
+			await updateDoc(currentUserRef, {
+				following: isFollowing ? arrayRemove(userId) : arrayUnion(userId),
+			});
+
+			await updateDoc(userToFollowOrUnfollowRef, {
+				followers: isFollowing ? arrayRemove(user.uid) : arrayUnion(user.uid),
+			});
+
+			if (isFollowing) {
+				//Unfollow
+				setUser({
+					...user,
+					following: user.following.filter((uid) => uid !== userId),
+				});
+				setUserProfile({
+					...userProfile,
+					followers: userProfile.followers.filter((uid) => uid !== user.uid),
+				});
+				localStorage.setItem(
+					'user-info',
+					JSON.stringify({
+						...user,
+						following: user.following.filter((uid) => uid !== userId),
+					})
+				);
+				setIsFollowing(false);
+			} else {
+				//Follow
+				setUser({
+					...user,
+					following: [...user.following, userId],
+				});
+				setUserProfile({
+					...userProfile,
+					followers: [...userProfile.followers, user.uid],
+				});
+				localStorage.setItem(
+					'user-info',
+					JSON.stringify({
+						...user,
+						following: [...user.following, userId],
+					})
+				);
+				setIsFollowing(true);
+			}
+		} catch (error) {
+			showToast('Error', error.message, 'error');
+		} finally {
+			setIsUpdating(false);
+		}
+	};
+
+	useEffect(() => {
+		if (user) {
+			const isFollowing = user.following.includes(userId);
+			setIsFollowing(isFollowing);
+		}
+	}, [user, userId]);
+
+	return { isUpdating, isFollowing, handleFollowUser };
+};
+
+export default useFollowUser;
