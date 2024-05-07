c0f753f631c15992a261a7f18574e2ad5cb8474f
Nicolas Horton <nic.horton00@gmail.com>
2024-05-07 15:47:55 -0500

    Set up to fetch profile when logged in

--- src/components/Profile/ProfileHeader.jsx
+++ src/components/Profile/ProfileHeader.jsx
@@ -1,6 +1,13 @@
 import { Avatar, AvatarGroup, Button, Flex, Text, VStack } from "@chakra-ui/react"
+import useUserProfileStore from "../../store/userProfileStore"
+import useAuthStore from "../../store/authStore";
 
 function ProfileHeader() {
+  const { userProfile } = useUserProfileStore();
+  const authUser = useAuthStore(state => state.user);
+  const visitingOwnProfileAndAuth = authUser && authUser.username === userProfile.username;
+  const visitingAnotherProfileAndAuth = authUser && authUser.username !== userProfile.username;
+
   return (
     <Flex gap={{base:4,sm:10}} py={10} direction={{base:"column",sm:"row"}}>
       <AvatarGroup
@@ -9,7 +16,7 @@ function ProfileHeader() {
         alignSelf={"flex-start"}
         mx={"auto"}
       >
-        <Avatar name="username" src="/profilepic.png" alt="User logo"/>
+        <Avatar src={userProfile.profilePicURL} alt="User logo"/>
       </AvatarGroup>
       <VStack alignItems={"start"} gap={2} mx={"auto"} flex={1}>
         <Flex gap={4} direction={{base:"column",sm:"row"}}
@@ -17,32 +24,43 @@ function ProfileHeader() {
           alignItems={"center"}
           w={"full"}
         >
-          <Text fontSize={{base:'sm',md:"lg"}}>Username</Text>
-          <Flex gap={4} alignItems={"center"} justifyContent={"center"}>
-            <Button bg={"white"} color={"black"} _hover={{bg:"whiteAlpha.800"}} size={{base:"xs",md:"sm"}}>
-              Edit Profile
-            </Button>
-          </Flex>
+          <Text fontSize={{base:'sm',md:"lg"}}>{userProfile.username}</Text>
+          {visitingOwnProfileAndAuth && (
+            <Flex gap={4} alignItems={"center"} justifyContent={"center"}>
+              <Button bg={"white"} color={"black"} _hover={{bg:"whiteAlpha.800"}} size={{base:"xs",md:"sm"}}>
+                Edit Profile
+              </Button>
+            </Flex>
+          )}
+
+          {visitingAnotherProfileAndAuth && (
+            <Flex gap={4} alignItems={"center"} justifyContent={"center"}>
+              <Button bg={"blue.500"} color={"white"} _hover={{bg:"blue.600"}} size={{base:"xs",md:"sm"}}>
+                Follow
+              </Button>
+            </Flex>
+          )}
+          
         </Flex>
 
         <Flex alignItems={"center"} gap={{base:2,sm:4}}>
           <Text fontSize={{base:"xs",md:"sm"}}>
-            <Text as={"span"} fontWeight={"bold"} mr={1}>4</Text>
+            <Text as={"span"} fontWeight={"bold"} mr={1}>{userProfile.posts.length}</Text>
             Posts
           </Text>
           <Text fontSize={{base:"xs",md:"sm"}}>
-            <Text as={"span"} fontWeight={"bold"} mr={1}>149</Text>
+            <Text as={"span"} fontWeight={"bold"} mr={1}>{userProfile.followers.length}</Text>
             Followers
           </Text>
           <Text fontSize={{base:"xs",md:"sm"}}>
-            <Text as={"span"} fontWeight={"bold"} mr={1}>175</Text>
+            <Text as={"span"} fontWeight={"bold"} mr={1}>{userProfile.following.length}</Text>
             Following
           </Text>
         </Flex>
         <Flex alignItems={"center"} gap={4}>
-          <Text fontSize={"sm"} fontWeight={"bold"}>Username</Text>
+          <Text fontSize={"sm"} fontWeight={"bold"}>{userProfile.fullName}</Text>
         </Flex>
-        <Text fontSize={"sm"}>This will be the little bio section with stuff about user</Text>
+        <Text fontSize={"sm"}>{userProfile.bio}</Text>
       </VStack>
     </Flex>
   )
--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,9 +1,17 @@
-import { Container, Flex } from "@chakra-ui/react"
+import { Container, Flex, Link, Skeleton, SkeletonCircle, Text, VStack } from "@chakra-ui/react"
 import ProfileHeader from "../../components/Profile/ProfileHeader"
 import ProfileTabs from "../../components/Profile/ProfileTabs"
 import ProfilePosts from "../../components/Profile/ProfilePosts"
+import useGetProfileByUsername from "../../hooks/useGetProfileByUsername"
+import { useParams, Link as RouterLink} from "react-router-dom"
 
 function ProfilePage() {
+  const { username }= useParams()
+  const { isLoading, userProfile } = useGetProfileByUsername(username)
+
+  const userNotFound = !isLoading && !userProfile
+  if(userNotFound) return <UserNotFound/>
+
   return (
     <Container maxW="container.lg" py={5}>
       <Flex
@@ -14,8 +22,10 @@ function ProfilePage() {
         mx={"auto"}
         flexDirection={"column"}
       >
-        <ProfileHeader/>
+        {!isLoading && userProfile && <ProfileHeader/>}
+        {isLoading && <ProfileHeaderSkeleton/>}
       </Flex>
+      
       <Flex
         px={{base:2,sm:4}}
         maxW={"full"}
@@ -32,3 +42,34 @@ function ProfilePage() {
 }
 
 export default ProfilePage
+
+const UserNotFound = () => {
+	return (
+		<Flex flexDir='column' textAlign={"center"} mx={"auto"}>
+			<Text fontSize={"2xl"}>User Not Found</Text>
+			<Link as={RouterLink} to={"/"} color={"blue.500"} w={"max-content"} mx={"auto"}>
+				Go home
+			</Link>
+		</Flex>
+	);
+};
+
+// skeleton for profile header
+const ProfileHeaderSkeleton = () => {
+	return (
+		<Flex
+			gap={{ base: 4, sm: 10 }}
+			py={10}
+			direction={{ base: "column", sm: "row" }}
+			justifyContent={"center"}
+			alignItems={"center"}
+		>
+			<SkeletonCircle size='24' />
+
+			<VStack alignItems={{ base: "center", sm: "flex-start" }} gap={2} mx={"auto"} flex={1}>
+				<Skeleton height='12px' width='150px' />
+				<Skeleton height='12px' width='100px' />
+			</VStack>
+		</Flex>
+	);
+};
