3c6d73390607cefc323531b0fe98cabd812de49b
Nicolas Horton <nic.horton00@gmail.com>
2024-05-07 11:43:27 -0500

    Added signup with email and password functionality

--- src/components/AuthForm/Signup.jsx
+++ src/components/AuthForm/Signup.jsx
@@ -1,6 +1,7 @@
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
-import { Input, Button, InputGroup, InputRightElement } from "@chakra-ui/react"
+import { Input, Button, InputGroup, InputRightElement, Alert, AlertIcon } from "@chakra-ui/react"
 import { useState } from "react";
+import useSignUpWithEmailPassword from "../../hooks/useSignUpWithEmailPassword";
 
 const Signup = () => {
   const [inputs, setInputs] = useState({
@@ -10,6 +11,7 @@ const Signup = () => {
     password:"",
   });
   const [showPassword, setShowPassword] = useState(false);
+  const {loading, error, signup} = useSignUpWithEmailPassword()
 
   return (
     <>
@@ -56,7 +58,17 @@ const Signup = () => {
           </InputRightElement>
         </InputGroup>
 
-        <Button w={"full"} colorScheme="blue" size={"sm"} fontSize={14}>
+        {error && (
+          <Alert status="error" fontSize={13} p={2} borderRadius={4}>
+            <AlertIcon fontSize={12}/>
+            {error.message}
+          </Alert>
+        )}
+
+        <Button w={"full"} colorScheme="blue" size={"sm"} fontSize={14} 
+          isLoading={loading}
+          onClick={() => signup(inputs)}
+        >
           Sign up
         </Button>
     </>
---
+++ src/hooks/useSignUpWithEmailPassword.js
@@ -0,0 +1,59 @@
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { auth, firestore } from '../firebase/firebase';
+import { doc, setDoc } from 'firebase/firestore';
+import useShowToast from './useShowToast';
+
+const useSignUpWithEmailPassword = () => {
+	const [createUserWithEmailAndPassword, , loading, error] =
+		useCreateUserWithEmailAndPassword(auth);
+
+	const showToast = useShowToast();
+
+	const signup = async (inputs) => {
+		if (
+			!inputs.email ||
+			!inputs.password ||
+			!inputs.username ||
+			!inputs.fullName
+		) {
+			showToast('Error', 'Please fill all the fields', 'error');
+			return;
+		}
+
+		try {
+			const newUser = await createUserWithEmailAndPassword(
+				inputs.email,
+				inputs.password
+			);
+
+			if (!newUser && error) {
+				showToast('Error', error.message, 'error');
+				return;
+			}
+
+			if (newUser) {
+				const userDoc = {
+					uid: newUser.user.uid,
+					email: inputs.email,
+					username: inputs.username,
+					fullName: inputs.fullName,
+					bio: '',
+					profilePicURL: '',
+					followers: [],
+					following: [],
+					posts: [],
+					createdAt: Date.now(),
+				};
+
+				await setDoc(doc(firestore, 'users', newUser.user.uid), userDoc);
+				localStorage.setItem('user-info', JSON.stringify(userDoc));
+			}
+		} catch (error) {
+			showToast('Error', error.message, 'error');
+		}
+	};
+
+	return { loading, error, signup };
+};
+
+export default useSignUpWithEmailPassword;
