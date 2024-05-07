ac1a8f0060092416e6c25c9b114d2c5851c1d10e
Nicolas Horton <nic.horton00@gmail.com>
2024-05-07 12:24:33 -0500

    Fixed bug so that sidebar renders only if not on auth page and a user is signed in

--- src/layouts/PageLayout/PageLayout.jsx
+++ src/layouts/PageLayout/PageLayout.jsx
@@ -1,13 +1,18 @@
 import { Flex, Box} from "@chakra-ui/react"
 import Sidebar from "../../components/Sidebar/Sidebar"
 import { useLocation } from "react-router-dom"
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from "../../firebase/firebase";
 
 function PageLayout({children}) {
   const {pathname} = useLocation()
+  const [user, loading, error] = useAuthState(auth);
+  const canRenderSidebar = pathname !== "/auth" && user;
+
   return (
     <Flex>
       {/* sidebar on left */}
-      {pathname !== '/auth' ? (
+      {canRenderSidebar ? (
         <Box w={{base:"70px",md:"240px"}}>
           <Sidebar />
         </Box>
