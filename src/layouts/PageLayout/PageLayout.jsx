89afb11ba3e746822ff1b6721cc88d1b85b9a900
Nicolas Horton <nic.horton00@gmail.com>
2024-05-07 12:39:44 -0500

    Refactored pageLayout to add Navbar when viewing a user without being logged in

--- src/layouts/PageLayout/PageLayout.jsx
+++ src/layouts/PageLayout/PageLayout.jsx
@@ -1,24 +1,33 @@
-import { Flex, Box} from "@chakra-ui/react"
+import { Flex, Box, Spinner} from "@chakra-ui/react"
 import Sidebar from "../../components/Sidebar/Sidebar"
 import { useLocation } from "react-router-dom"
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from "../../firebase/firebase";
+import Navbar from "../../components/Navbar/Navbar";
 
 function PageLayout({children}) {
   const {pathname} = useLocation()
-  const [user, loading, error] = useAuthState(auth);
+  const [user, loading, ] = useAuthState(auth);
   const canRenderSidebar = pathname !== "/auth" && user;
+  const canRenderNavbar = !user && !loading && pathname !== "/auth";
+  const checkingIfUserIsAuth = !user && loading
+
+  if(checkingIfUserIsAuth) return <PageLayoutSpinner/>
 
   return (
-    <Flex>
+    <Flex flexDir={canRenderNavbar ? "column" : "row"}>
       {/* sidebar on left */}
       {canRenderSidebar ? (
         <Box w={{base:"70px",md:"240px"}}>
           <Sidebar />
         </Box>
       ) : null}
+
+      {/* Navbar */}
+      {canRenderNavbar ? <Navbar /> : null}
+
       {/* page content on right */}
-      <Box flex={1} w={{base:"calc(100% - 70px)",md:"calc(100% - 240px)"}}>
+      <Box flex={1} w={{base:"calc(100% - 70px)",md:"calc(100% - 240px)"}} mx={"auto"}>
         {children}
       </Box>
     </Flex>
@@ -26,3 +35,11 @@ function PageLayout({children}) {
 }
 
 export default PageLayout
+
+const PageLayoutSpinner = () => {
+	return (
+		<Flex flexDir='column' h='100vh' alignItems='center' justifyContent='center'>
+			<Spinner size='xl' />
+		</Flex>
+	);
+};
