8b1cb0da4fdba4a5fcbc8a3dc3276ef7ca0842f7
Nicolas Horton <nic.horton00@gmail.com>
2024-05-08 10:29:27 -0500

    Added create a post functionality

--- src/store/userProfileStore.js
+++ src/store/userProfileStore.js
@@ -3,7 +3,14 @@ import { create } from 'zustand';
 const useUserProfileStore = create((set) => ({
 	userProfile: null,
 	setUserProfile: (userProfile) => set({ userProfile }),
-	// addPost:()
+	//Used to update posts number on profile page
+	addPost: (post) =>
+		set((state) => ({
+			userProfile: {
+				...state.userProfile,
+				posts: [post.id, ...state.userProfile.posts],
+			},
+		})),
 }));
 
 export default useUserProfileStore;
