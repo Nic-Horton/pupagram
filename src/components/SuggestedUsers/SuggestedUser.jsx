c1e66973dce4828228f0b9d52c879b16b40b24d0
Nicolas Horton <nic.horton00@gmail.com>
2024-05-08 16:30:12 -0500

    Added links to profiles that are in suggested users

--- src/components/SuggestedUsers/SuggestedUser.jsx
+++ src/components/SuggestedUsers/SuggestedUser.jsx
@@ -1,6 +1,7 @@
 import { Avatar, Box, Button, Flex, VStack } from "@chakra-ui/react"
 import useFollowUser from "../../hooks/useFollowUser"
 import useAuthStore from "../../store/authStore";
+import { Link } from "react-router-dom";
 
 function SuggestedUser({ user, setUser }) {
   const { isFollowing, isUpdating, handleFollowUser } = useFollowUser(user.uid);
@@ -19,11 +20,15 @@ function SuggestedUser({ user, setUser }) {
   return (
     <Flex justifyContent={"space-between"} alignItems={"center"} w={"full"}>
       <Flex alignItems={"center"} gap={2}>
-        <Avatar src={user.profilePicURL} size={"md"}/>
+        <Link to={`/${user.username}`}>
+					<Avatar src={user.profilePicURL} size={"md"} />
+				</Link>
         <VStack spacing={2} alignItems={"flex-start"}>
-          <Box fontSize={12} fontWeight={"bold"}>
-            {user.fullName}
-          </Box>
+          <Link to={`/${user.username}`}>
+						<Box fontSize={12} fontWeight={"bold"}>
+							{user.fullName}
+						</Box>
+					</Link>
           <Box fontSize={11} color={"gray.500"}>
             {user.followers.length} followers
           </Box>
