6ca7282d95a5debd4577d3bfc4aae75e9a82b45c
Nicolas Horton <nic.horton00@gmail.com>
2024-05-10 10:58:08 -0500

    Made an adjustment to size of Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
 	return (
 		<>
-		<Container maxW={"container.lg"} my={4}>
-			<Flex w={"full"} justifyContent={{ base: "center", sm: "space-between" }} alignItems={"center"}>
+		<Container maxW={"container.lg"} my={1} position={"sticky"}>
+			<Flex w={"full"} justifyContent={{ base: "center", sm: "space-between" }} alignItems={"center"} >
 				<Link to='/auth'>
 					<Flex display={{ base: "none", sm: "block" }} cursor={"pointer"}>
-					<Image src='/pupagramLogo.png' boxSize='150px' display={{ base: "none", sm: "block" }} cursor={"pointer"} />
+					<Image src='/pupagramLogo.png' boxSize='65px' display={{ base: "none", sm: "block" }} cursor={"pointer"} />
 					</Flex>
 				</Link>
-				<Flex gap={4} height={100} alignItems={"center"}>
+				<Flex gap={4} height={65} alignItems={"center"}>
 					<Link to='/auth'>
 						<Button colorScheme={"orange"} size={"sm"}>
 							Login
